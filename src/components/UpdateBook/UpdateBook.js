5c853c25173c3df8147c2090d0aba00df3277c80
Fernanda Araujo <nanda.arf@gmail.com>
2017-09-19 19:01:15 -0300

    refactor: Restructures the UpdateBook component code

--- src/components/UpdateBook/UpdateBook.js
+++ src/components/UpdateBook/UpdateBook.js
@@ -2,26 +2,42 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import * as BooksAPI from '../../utilities/BooksAPI';
 
-const UpdateBook = ({ bookID }) => {
-  const updateBook = (bookStatus) => {
-    BooksAPI.update(bookStatus, bookID);
-  };
+class UpdateBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookStatus: this.props.shelf
+    };
 
-  return (
-    <div className="book-shelf-changer">
-      <select>
-        <option value="none" disabled>Move to...</option>
-        <option value="currentlyReading" onChange={event => this.updateBook(event.target.value)}>Currently Reading</option>
-        <option value="wantToRead" onChange={event => this.updateBook(event.target.value)}>Want to Read</option>
-        <option value="read" onChange={event => this.updateBook(event.target.value)}>Read</option>
-        <option value="none" onChange={event => this.updateBook(event.target.value)}>None</option>
-      </select>
-    </div>
-  );
-};
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      bookStatus: event.target.value
+    });
+
+    BooksAPI.update(this.props.bookID, this.state.bookStatus);
+  }
+
+  render() {
+    return (
+      <div className="book-shelf-changer">
+        <select value={this.state.value} onChange={this.handleChange}>
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    );
+  }
+}
 
 UpdateBook.propTypes = {
-  bookID: PropTypes.string
+  shelf: PropTypes.string,
+  bookID: PropTypes.string // id
 };
 
 export default UpdateBook;
