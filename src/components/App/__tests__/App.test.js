d0e7d6bd86f1c76d98866decdf2c51409680d500
Fernanda Araujo <nanda.arf@gmail.com>
2017-09-24 19:16:18 -0300

    test: Creates test to app for check the routes

--- src/components/App/__tests__/App.test.js
+++ src/components/App/__tests__/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../../App/App';
+import { shallow, mount } from 'enzyme';
+import localStorage from 'mock-local-storage';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import App from '../App';
+
+global.window = {};
+window.localStorage = global.localStorage;
+
+describe('test app sections renders', () => {
+  it('should render components per route', () => {
+    const app = shallow(<App />);
+    expect(app.find('Route').at(0).props().path).toBe('/');
+    expect(app.find('Route').at(1).props().path).toBe('/search');
+  });
 });
