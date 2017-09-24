dd0bea60624f104dcaf5c160ba10f1d65b22118d
Fernanda Araujo <nanda.arf@gmail.com>
2017-09-24 18:45:38 -0300

    test: Changes book section test using before each for remove duplicate code

--- src/components/BookSection/__tests__/BookSection.test.js
+++ src/components/BookSection/__tests__/BookSection.test.js
@@ -8,23 +8,28 @@ global.window = {};
 window.localStorage = global.localStorage;
 
 describe('test book section component', () => {
-  const props = {
-    books: [{
-      id: 'nggnmAEACAAJ',
-      shelf: 'currentlyReading',
-      subtitle: 'A Complete Introduction',
-      title: 'The Linux Command Line'
-    }, {
-      id: 'pggnmAEACAAJ',
-      shelf: 'wantToRead',
-      subtitle: 'A Complete Introduction 2',
-      title: 'The Linux Command Line 2'
-    }],
-    status: 'wantToRead',
-    titleSection: 'Want To Read'
-  };
+  let props;
+  let updateBook;
 
-  const updateBook = shallow(<BookSection {...props} />);
+  beforeEach(() => {
+    props = {
+      books: [{
+        id: 'nggnmAEACAAJ',
+        shelf: 'currentlyReading',
+        subtitle: 'A Complete Introduction',
+        title: 'The Linux Command Line'
+      }, {
+        id: 'pggnmAEACAAJ',
+        shelf: 'wantToRead',
+        subtitle: 'A Complete Introduction 2',
+        title: 'The Linux Command Line 2'
+      }],
+      status: 'wantToRead',
+      titleSection: 'Want To Read'
+    };
+
+    updateBook = shallow(<BookSection {...props} />);
+  });
 
   it('should render book section for Want to Read books', () => {
     expect(updateBook.find('.bookshelf').exists()).toBe(true);
