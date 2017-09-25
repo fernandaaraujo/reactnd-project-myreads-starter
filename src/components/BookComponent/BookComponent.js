19987418811a3ec10c381bfed2ad66ddbf2d3dc7
Fernanda Araujo <nanda.arf@gmail.com>
2017-09-25 19:26:52 -0300

    feat: Adds option to shows book details on list books

--- src/components/BookComponent/BookComponent.js
+++ src/components/BookComponent/BookComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import UpdateBook from '../UpdateBook/UpdateBook';
 
 import './BookComponent.css';
@@ -13,6 +14,7 @@ const BookComponent = ({ book }) => {
         <div className="book-top">
           <div className="book-cover" style={{ backgroundImage: `url(${bookImage})` }} />
           <UpdateBook book={book} />
+          <Link className="book-details" to={`/details/${book.id}`} />
         </div>
         <div className="book-title">{book.title}</div>
         <div className="book-authors">{book.authors}</div>
