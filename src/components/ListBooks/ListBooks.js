73533f60877f2ce40c5c572ba641e153d0645c37
Fernanda Araujo <nanda.arf@gmail.com>
2017-09-29 13:19:49 -0300

    refactor: Transforms list books in functional stateless component

--- src/components/ListBooks/ListBooks.js
+++ src/components/ListBooks/ListBooks.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
@@ -6,29 +6,25 @@ import BookSection from '../BookSection/BookSection';
 
 import './ListBooks.css';
 
-class ListBooks extends Component {
-  render() {
-    const { books } = this.props;
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <BookSection books={books} status="currentlyReading" titleSection="Currently Reading" />
-            <BookSection books={books} status="wantToRead" titleSection="Want to Read" />
-            <BookSection books={books} status="read" titleSection="Read" />
-          </div>
-        </div>
-        <Link
-          to="/search"
-          className="open-search">Add a book</Link>
+const ListBooks = ({ books }) => {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    );
-  }
-}
+      <div className="list-books-content">
+        <div>
+          <BookSection books={books} status="currentlyReading" titleSection="Currently Reading" />
+          <BookSection books={books} status="wantToRead" titleSection="Want to Read" />
+          <BookSection books={books} status="read" titleSection="Read" />
+        </div>
+      </div>
+      <Link
+        to="/search"
+        className="open-search">Add a book</Link>
+    </div>
+  );
+};
 
 ListBooks.propTypes = {
   books: PropTypes.array
